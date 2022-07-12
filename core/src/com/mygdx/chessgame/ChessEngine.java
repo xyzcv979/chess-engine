3c0514ec5cf200b9f505988cc2fa9dd9f0d2ef8d
Alex Ng <avon1462@gmail.com>
2022-07-11 22:06:11 -0700

    Added chess board

--- core/src/com/mygdx/chessgame/ChessEngine.java
+++ core/src/com/mygdx/chessgame/ChessEngine.java
@@ -1,31 +1,43 @@
 package com.mygdx.chessgame;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class ChessEngine extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	Texture texture;
+	TextureRegion mainBackground;
+	float  stageHeight, stageWidth;
+
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		texture = new Texture("chess-board.jpg");
+		mainBackground = new TextureRegion(texture,0, 0, 690, 690);
+//		stageHeight = Gdx.graphics.getHeight();
+//		stageWidth = Gdx.graphics.getWidth();
+
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		ScreenUtils.clear(0, 0, 1, 1);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(mainBackground, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		texture.dispose();
 	}
 }
